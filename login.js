5a272c44fed2daca1cae399d76084db4e1dd8a4d
madanenikalyan <112858372+madanenikalyan@users.noreply.github.com>
2024-07-27 11:05:29 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,10 +1,10 @@
 var usernameInput= document.getElementById("username");
-var username = usernameInput.ariaValueMax;
+var username = usernameInput.ValueMax;
 
 //Display the user name under the profile picture
 
 var usernameDisplay = document.createElement("div");
 usernameDisplay.innerHTML = username;
 usernameDisplay.classList.add("username");
-document.querySelector(".profile").appendChild(usernameDisplay);
+document.querySelector(".profile").appendChild(usernameDisplay) inner.HTML;
 
