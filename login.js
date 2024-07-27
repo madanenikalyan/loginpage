9a52056324d9635b8cd202bc901dde1d250a8cb3
madanenikalyan <112858372+madanenikalyan@users.noreply.github.com>
2024-07-27 10:58:41 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,10 @@
+var usernameInput= document.getElementById("username");
+var username = usernameInput.ariaValueMax;
+
+//Display the user name under the profile picture
+
+var usernameDisplay = document.createElement("div");
+usernameDisplay.innerHTML = username;
+usernameDisplay.classList.add("username");
+document.querySelector(".profile").appendChild(usernameDisplay);
+
